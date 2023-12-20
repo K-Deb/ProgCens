0a4f8d0a7ce70baa3491ec7b729ad412a216acc5
K-Deb <debayanbw.3@gmail.com>
2023-12-20 14:04:05 +0530

    function name change

--- R/GRD_CDF.R
+++ R/GRD_CDF.R
@@ -12,7 +12,7 @@
 #' @export
 #'
 #' @examples
-pgr = function(q, shape, scale = 1, lower.tail=TRUE, log.p=FALSE)
+pGR = function(q, shape, scale = 1, lower.tail=TRUE, log.p=FALSE)
 {
   cdf = (1-exp(-scale^2*q^2))^shape
   if(isTRUE(lower.tail)==T & isTRUE(log.p)==F)
