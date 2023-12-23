283315e1ec71fe971cb6373206fdffd102e46ab5
K-Deb <debayanbw.3@gmail.com>
2023-12-24 00:44:17 +0530

    progressive first-failure sample generation

--- R/rpffc2.R
+++ R/rpffc2.R
@@ -1,20 +1,22 @@
-#' Simulating a progressively type-II censored sample from a univariate distribution
+#' Simulating a progressively first-failure type-II censored sample from a univariate distribution
 #'
-#' @param n Total number of items placed on test at time zero
+#' @param n Total number of groups placed on test at time zero
 #' @param m Number of required failures
+#' @param k Number of items in a group
 #' @param R Prefixed censoring scheme
 #' @param prob Probability of removal
 #' @param QF Quantile function of the univariate distribution
 #' @param ... Further arguments to be passed to \code{QF}
 #' @param set_seed Random number generation seed
 #'
-#' @return A dataframe with progressively type-II censored sample from a univariate distribution and the censoring scheme
+#' @return A dataframe with progressively type-II first-failure censored sample from a univariate distribution and the censoring scheme
 #' @export
+#'
 #' @examples
-rpc2 = function(n, m = NULL, R = NULL, prob = NULL, QF, ..., set_seed = NULL)
+rpffc2 = function(n, m = NULL, k, R = NULL, prob = NULL, QF, ..., set_seed = NULL)
 {
   stopifnot("QF must be a quantile function" = is.function(QF))
   U = rpc2unif(n = n, m = m, R = R, prob = prob, set_seed = set_seed)
-  X = QF(U[1,], ...)
+  X = QF((1-(1-U[1,])^(1/k)), ...)
   return(rbind(X = X, R = U[2,]))
 }
