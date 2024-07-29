c6792e7b6fac79cc9ca06e06eac8c78206b12382
K-Deb <debayanbw.3@gmail.com>
2024-07-29 18:15:36 +0530

    generalized Lindley distribution

---
+++ R/g_Lindley.R
@@ -0,0 +1,129 @@
+#' The Generalized Lindley Distribution
+#'
+#' @param x,q Vector of quantiles.
+#' @param p vector of probabilities.
+#' @param n number of observations.
+#' @param shape shape parameter.
+#' @param rate  rate parameter
+#' @param lower.tail logical; if \code{TRUE}
+#' @param log,log.p logical
+#' @name gLindley
+NULL
+#> NULL
+#' @return later
+#' @importFrom stats uniroot
+#' @export
+#'
+#' @examples
+
+#' @rdname gLindley
+#' @export
+dgLindley = function (x, shape = 1, rate, log = FALSE)
+{
+  denslog = log(shape) + (2 * log(rate)) - log1p(rate) + log1p(x) +
+    ((shape - 1) * log1p(- ((1 + rate + (rate * x)) / (1 + rate)) * exp(- rate * x))) - (rate * x)
+  denslog[x < 0] = -Inf
+  denslog[(shape < 0) | (rate < 0)] = NaN
+  denslog[(shape == 0) | (x == 0) | (rate == 0)] = -Inf
+  if (log) {
+    return((denslog))
+  }
+  else {
+    return(exp((denslog)))
+  }
+}
+#' @rdname gLindley
+#' @export
+pgLindley = function (q, shape = 1, rate, lower.tail = TRUE, log.p = FALSE)
+{
+  if (lower.tail) {
+    if (log.p) {
+      distfun = shape * log1p(- ((1 + rate + (rate * q)) / (1 + rate)) * exp(- rate * q))
+      distfun[q <= 0] = -Inf
+    }
+    else {
+      distfun = (1 - (((1 + rate + (rate * q)) / (1 + rate)) * exp(- rate * q))) ^ shape
+      distfun[q <= 0] = 0
+    }
+  }
+  else {
+    if (log.p) {
+      distfun = log1p(- (1 - (((1 + rate + (rate * q)) / (1 + rate)) * exp(- rate * q))) ^ shape)
+      distfun[q <= 0] = 0
+    }
+    else {
+      distfun = 1 - (1 - (((1 + rate + (rate * q)) / (1 + rate)) * exp(- rate * q))) ^ shape
+      distfun[q <= 0] = 1
+    }
+  }
+  distfun[(shape < 0) | (rate < 0)] = NaN
+  return(distfun)
+}
+#' @rdname gLindley
+#' @export
+qgLindley = function (p, shape = 1, rate, lower.tail = TRUE, log.p = FALSE)
+{
+  if (lower.tail) {
+    if (log.p) {
+      Obj = function(x, shape, rate, p)
+      {
+        return(pgLindley(x, shape, rate) - exp(p))
+      }
+      qfun = c()
+      for(i in 1 : length(p))
+      {
+        qfun[i] = uniroot(Obj, interval = c(- 1e5, 1e5), shape = shape, rate = rate, p = p[i])$root
+      }
+      qfun[p > 0] = NaN
+    }
+    else {
+      Obj = function(x, shape, rate, p)
+      {
+        return(pgLindley(x, shape, rate) - p)
+      }
+      qfun = c()
+      for(i in 1 : length(p))
+      {
+        qfun[i] = uniroot(Obj, interval = c(- 1e5, 1e5), shape = shape, rate = rate, p = p[i])$root
+      }
+      qfun[p < 0] = NaN
+      qfun[p > 1] = NaN
+    }
+  }
+  else {
+    if (log.p) {
+      Obj = function(x, shape, rate, p)
+      {
+        return(pgLindley(x, shape, rate, lower.tail = F) - exp(p))
+      }
+      qfun = c()
+      for(i in 1 : length(p))
+      {
+        qfun[i] = uniroot(Obj, interval = c(- 1e5, 1e5), shape = shape, rate = rate, p = p[i])$root
+      }
+      qfun[p > 0] = NaN
+    }
+    else {
+      Obj = function(x, shape, rate, p)
+      {
+        return(pgLindley(x, shape, rate, lower.tail = F) - p)
+      }
+      qfun = c()
+      for(i in 1 : length(p))
+      {
+        qfun[i] = uniroot(Obj, interval = c(- 1e5, 1e5), shape = shape, rate = rate, p = p[i])$root
+      }
+      qfun[p < 0] = NaN
+      qfun[p > 1] = NaN
+    }
+  }
+  qfun[(shape < 0) | (rate < 0)] = NaN
+  return(qfun)
+}
+#' @rdname gLindley
+#' @export
+rgLindley = function(n, shape = 1, rate)
+{
+  out = qgLindley(runif(n), shape = shape, rate = rate)
+  return(out)
+}
